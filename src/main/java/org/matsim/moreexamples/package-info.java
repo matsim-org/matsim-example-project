f0ae0c98797a27b8d4ecb706b79bf2870fecae21
kainagel <nagel@vsp.tu-berlin.de>
2016-08-06 20:43:29 +0200

    try tell maven to download sources and javadocs at least for eclipse, but I can't say if it is working

---
+++ src/main/java/org/matsim/moreexamples/package-info.java
@@ -0,0 +1,25 @@
+/* *********************************************************************** *
+ * project: org.matsim.*												   *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2008 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+/**
+ * Examples are collected in the package {@link tutorial}, in particular {@link tutorial.programming}.  They sit physically in the
+ * matsim main repository, not on the matsim-example-project on which you are working here.  However, maven should be configured
+ * such that it downloads the matsim sources as well, so you should be able to click on the package links above 
+ *
+ */
+package org.matsim.moreexamples;
