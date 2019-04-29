63be8e8b21e3814581b0c5cdfa70dc9bc6ba61ba
kainagel <nagel@vsp.tu-berlin.de>
2019-04-29 08:07:54 +0200

    Revert "complete the rename"
    This reverts commit b3d96dc03503dd36d475407d41abf6f077b7b52c.

--- src/main/java/org/matsim/run/RunMATSim.java
+++ src/main/java/org/matsim/run/RunMATSim.java
@@ -30,7 +30,7 @@ import org.matsim.core.scenario.ScenarioUtils;
  * @author nagel
  *
  */
-public class RunMATSim {
+public class RunMatsim {
 
 	public static void main(String[] args) {
 		Gbl.assertIf(args.length >=1 && args[0]!="" );
