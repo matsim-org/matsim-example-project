0fba817a9f065a004e43b9453d911793e0113e94
kainagel <nagel@vsp.tu-berlin.de>
2019-12-09 16:26:14 +0100

    add population comparison test

--- src/test/java/org/matsim/project/RunMatsimTest.java
+++ src/test/java/org/matsim/project/RunMatsimTest.java
@@ -22,17 +22,22 @@ import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.events.EventsUtils;
+import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.examples.ExamplesUtils;
 import org.matsim.testcases.MatsimTestUtils;
 import org.matsim.utils.eventsfilecomparison.EventsFileComparator;
 
+import java.net.URL;
+
 /**
  * @author nagel
  *
  */
-public class RunMatsimTest{
+public class RunMatsimTest {
 	
 	@Rule public MatsimTestUtils utils = new MatsimTestUtils() ;
 
@@ -40,18 +45,29 @@ public class RunMatsimTest{
 	public final void test() {
 
 		try {
-			String [] args = { IOUtils.extendUrl( ExamplesUtils.getTestScenarioURL( "equil" ) , "config.xml" ).toString(),
+			final URL baseUrl = ExamplesUtils.getTestScenarioURL( "equil" );
+			final String fullUrl = IOUtils.extendUrl( baseUrl, "config.xml" ).toString();
+			String [] args = {fullUrl,
 				  "--config:controler.outputDirectory", utils.getOutputDirectory(),
-				  "--config:controler.lastIteration=1"
+				  "--config:controler.lastIteration", "2"
 			} ;
 			RunMatsim.main( args ) ;
 			{
-				String expectedEventsFile = utils.getInputDirectory() + "/output_events.xml.gz" ;
-				String actualEventsFile = utils.getOutputDirectory() + "/output_events.xml.gz" ;
-				EventsFileComparator.Result result = EventsUtils.compareEventsFiles( expectedEventsFile, actualEventsFile );;
+				Population expected = PopulationUtils.createPopulation( ConfigUtils.createConfig() ) ;
+				PopulationUtils.readPopulation( expected, utils.getInputDirectory() + "/output_plans.xml.gz" );
+
+				Population actual = PopulationUtils.createPopulation( ConfigUtils.createConfig() ) ;
+				PopulationUtils.readPopulation( actual, utils.getOutputDirectory() + "/output_plans.xml.gz" );
+
+				boolean result = PopulationUtils.comparePopulations( expected, actual );
+				Assert.assertTrue( result );
+			}
+			{
+				String expected = utils.getInputDirectory() + "/output_events.xml.gz" ;
+				String actual = utils.getOutputDirectory() + "/output_events.xml.gz" ;
+				EventsFileComparator.Result result = EventsUtils.compareEventsFiles( expected, actual );
 				Assert.assertEquals( EventsFileComparator.Result.FILES_ARE_EQUAL, result );
 			}
-
 
 		} catch ( Exception ee ) {
 			Logger.getLogger(this.getClass()).fatal("there was an exception: \n" + ee ) ;
