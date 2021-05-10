b2b57a4efdf38e4e545b70552ac78ad01a50ad2f
Janekdererste <laudan@tu-berlin.de>
2021-05-10 16:40:51 +0200

    Add yet another helpful message

--- src/main/java/org/matsim/analysis/TestCommit.java
+++ src/main/java/org/matsim/analysis/TestCommit.java
@@ -4,5 +4,7 @@ public class TestCommit {
 
     public static void main(String[] args) {
         System.out.println("Hello matsim seminar");
+
+        System.out.println("the other print statement");
     }
 }
