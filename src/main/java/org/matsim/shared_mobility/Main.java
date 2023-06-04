58962c35c544023534faa1603850eb9f1ee0b915
Luca Castello <Luca Castello@Carboni-PC>
2023-06-04 16:39:32 -0400

    drt sample scenario from github example to test

--- src/main/java/org/matsim/shared_mobility/Main.java
+++ src/main/java/org/matsim/shared_mobility/Main.java
@@ -3,6 +3,9 @@ package org.matsim.shared_mobility;
 import org.apache.commons.configuration.ConfigurationUtils;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
+import org.matsim.contrib.drt.run.DrtControlerCreator;
+import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
 import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
 import org.matsim.contrib.dvrp.run.DvrpModule;
 import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
@@ -17,6 +20,7 @@ import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.vehicles.Vehicle;
 import org.matsim.vehicles.VehicleType;
 import org.matsim.vehicles.VehicleUtils;
+import org.matsim.vis.otfvis.OTFVisConfigGroup;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
 public class Main {
 
@@ -26,8 +30,10 @@ public class Main {
 		
 		String test = "scenarios/siouxfalls-2014/test/config.xml";
 		String filename = "scenarios/siouxfalls-2014/config.xml";
+		String vehicles = "scenarios/siouxfalls-2014/vehicles_100.xml";
+		String drt = "scenarios/drt/config.xml";
 		
-		Config config = ConfigUtils.loadConfig(test,  new DvrpConfigGroup(), new MultiModeTaxiConfigGroup());
+		Config config = ConfigUtils.loadConfig(drt,  new DvrpConfigGroup(), new MultiModeDrtConfigGroup(), new OTFVisConfigGroup());
 		config.qsim().setSimStarttimeInterpretation(StarttimeInterpretation.onlyUseStarttime);
 		config.controler().setLastIteration(0);
 		config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
@@ -35,21 +41,38 @@ public class Main {
 		
 		  Scenario scenario = ScenarioUtils.loadScenario(config);
 		  
+			/*
+			 * var drtConfig = MultiModeDrtConfigGroup.get(config).getModalElements();
+			 * for(DrtConfigGroup drtConfig1 : drtConfig) {
+			 * 
+			 * var veh = drtConfig1.createParameterSet(vehicles);
+			 * 
+			 * }
+			 */
 		  
-		  VehicleType avType = VehicleUtils.createVehicleType(Id.create("autonomousVehicleType",VehicleType.class ) );
-		  avType.setFlowEfficiencyFactor(2.0);
-		  scenario.getVehicles().addVehicleType(avType);
 		  
-		  for (int i = 0; i < scenario.getPopulation().getPersons().size(); i++) {
-		  //agents on lower route get AVs as vehicles, agents on upper route keep a standard vehicle (= default, if nothing is set)
-		  Id<Vehicle> vid = Id.createVehicleId("AV_" + i); 
-		  Vehicle v = scenario.getVehicles().getFactory().createVehicle(vid, avType);
-		  scenario.getVehicles().addVehicle(v); }
-		  
-		  Controler controler = new Controler(scenario);
-		  controler.addOverridingModule(new DvrpModule());
-		  controler.addOverridingModule(new MultiModeTaxiModule());
-		  controler.configureQSimComponents(DvrpQSimComponents.activateAllModes(MultiModeTaxiConfigGroup.get(config)));
+			/*
+			 * VehicleType avType =
+			 * VehicleUtils.createVehicleType(Id.create("autonomousVehicleType",VehicleType.
+			 * class ) ); avType.setFlowEfficiencyFactor(2.0);
+			 * scenario.getVehicles().addVehicleType(avType);
+			 * 
+			 * for (int i = 0; i < scenario.getPopulation().getPersons().size(); i++) {
+			 * //agents on lower route get AVs as vehicles, agents on upper route keep a
+			 * standard vehicle (= default, if nothing is set) Id<Vehicle> vid =
+			 * Id.createVehicleId("AV_" + i); Vehicle v =
+			 * scenario.getVehicles().getFactory().createVehicle(vid, avType);
+			 * scenario.getVehicles().addVehicle(v); }
+			 */
+		  boolean otfvis = false;
+		  Controler controler = DrtControlerCreator.createControler(config, otfvis);
+			/*
+			 * Controler controler = new Controler(scenario);
+			 * controler.addOverridingModule(new DvrpModule());
+			 * controler.addOverridingModule(new MultiModeTaxiModule());
+			 * controler.configureQSimComponents(DvrpQSimComponents.activateAllModes(
+			 * MultiModeTaxiConfigGroup.get(config)));
+			 */
 		  controler.run();
 		 
 
