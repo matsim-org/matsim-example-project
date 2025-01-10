50d4e12e5e2b9d3ac6aeefda56a649f727cd5f82
gustav <gustav.p.andre@gmail.com>
2025-01-10 13:47:11 +0100

    lade till XmlParser

---
+++ src/main/java/org/matsim/project/XmlParser.java
@@ -0,0 +1,125 @@
+package org.matsim.project;
+
+import scala.xml.XML;
+
+import javax.xml.stream.*;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
+public class XmlParser {
+
+    public static void main(String[] args) throws XMLStreamException, FileNotFoundException {
+        String path = "C:\\Users\\gusta\\Documents\\Exjobb\\Workspaces\\matsim-example-project\\test\\input\\org\\matsim\\evDetour\\1pctNetwork.xml";
+
+        ArrayList<StartElement> links = XmlParser.findTagsInXMLFile(path, "link");
+        String[] linkIDs = new String[links.size()];
+        for(int i = 0; i < links.size(); i++){
+            linkIDs[i] = XmlParser.getValueFromTag(links.get(i), "id");
+            System.out.println(linkIDs[i]);
+        }
+
+        Random rand = new Random();
+        int workIdIndex = 0;
+        int homeIdIndex = 0;
+        while(workIdIndex == homeIdIndex){
+            workIdIndex = rand.nextInt(linkIDs.length);
+            homeIdIndex = rand.nextInt(linkIDs.length);
+        }
+        String newWorkId = linkIDs[workIdIndex];
+        String newHomeId = linkIDs[homeIdIndex];
+
+
+        String plansPath = "C:\\Users\\gusta\\Documents\\Exjobb\\Workspaces\\matsim-example-project\\test\\input\\org\\matsim\\evDetour\\triple-charger-plan.xml";
+        String outputTempPath = "C:\\Users\\gusta\\Documents\\Exjobb\\Workspaces\\matsim-example-project\\test\\input\\org\\matsim\\evDetour\\triple-charger-plan-test.xml";
+        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        XMLOutputFactory xmlOutputFactory = XMLOutputFactory.newInstance();
+
+        XMLEventReader reader = xmlInputFactory.createXMLEventReader(new FileInputStream(plansPath));
+        XMLEventWriter writer = xmlOutputFactory.createXMLEventWriter(new FileOutputStream(plansPath));
+
+        while(reader.hasNext()){
+            XMLEvent nextEvent = reader.nextEvent();
+            if(nextEvent.isStartElement()) {
+                StartElement startElement = nextEvent.asStartElement();
+                if (startElement.getName().getLocalPart().equals("activity")) {
+                    Iterator<Attribute> attributes = startElement.getAttributes();
+                    while(attributes.hasNext()){
+                        Attribute at = attributes.next();
+                        if(at.getName().getLocalPart().equals("type")){
+                            if(at.getValue().equals("work")){
+                                writer.add(modifyAndReturnStartElement(startElement, "link", newWorkId));
+                            } else if (at.getValue().equals("home")) {
+                                writer.add(modifyAndReturnStartElement(startElement, "link", newHomeId));
+                            }
+                            break;
+                        }
+                    }
+                } else{
+                    writer.add(startElement);
+                }
+            } else {
+                writer.add(nextEvent);
+            }
+        }
+
+        reader.close();
+        writer.close();
+
+    }
+
+    public static ArrayList<StartElement> findTagsInXMLFile(String filePath, String tagName) throws FileNotFoundException, XMLStreamException {
+        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        XMLEventReader reader = xmlInputFactory.createXMLEventReader(new FileInputStream(filePath));
+
+        ArrayList<StartElement> foundTags = new ArrayList<>();
+        while(reader.hasNext()){
+            XMLEvent nextEvent = reader.nextEvent();
+            if (nextEvent.isStartElement()) {
+                StartElement startElement = nextEvent.asStartElement();
+                if(startElement.getName().getLocalPart().equals(tagName)){
+                    foundTags.add(startElement);
+                }
+            }
+        }
+        return foundTags;
+    }
+
+    public static String getValueFromTag(StartElement tag, String valName){
+        Iterator<Attribute> attributes = tag.getAttributes();
+        while(attributes.hasNext()){
+            Attribute at = attributes.next();
+            if(at.getName().getLocalPart().equals(valName)){
+                return at.getValue();
+            }
+
+        }
+        return "not found";
+
+    }
+
+    private static StartElement modifyAndReturnStartElement(StartElement startElement, String attributeToEditName, String newValue){
+
+        Iterator<Attribute> attributes = startElement.getAttributes();
+        ArrayList<Attribute> updatedAttributes = new ArrayList<>();
+
+        XMLEventFactory eventFactory = XMLEventFactory.newInstance();
+
+        while(attributes.hasNext()){
+            Attribute at = attributes.next();
+            if(at.getName().getLocalPart().equals(attributeToEditName)){
+                updatedAttributes.add(eventFactory.createAttribute(attributeToEditName, newValue));
+            } else {
+                updatedAttributes.add(at);
+            }
+        }
+
+        return eventFactory.createStartElement(startElement.getName(), updatedAttributes.iterator(), startElement.getNamespaces());
+    }
+}
